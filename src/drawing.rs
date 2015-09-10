31be32a8afbc0fa98316f5940bd6e5032cdeb25f
Allen Welkie <allen@localhost.localdomain>
2015-09-09 20:42:46 -0400

    Moved drawing functions to separate file.

---
+++ src/drawing.rs
@@ -0,0 +1,116 @@
+use std::char;
+use num::Complex;
+use rustty::{Terminal, Cell};
+
+// indexing is from the top of the cell
+fn pixel_nums_to_braille(p1: Option<u8>, p2: Option<u8>) -> char {
+    let pixel_map = [[0x01, 0x08],
+                     [0x02, 0x10],
+                     [0x04, 0x20],
+                     [0x40, 0x80]];
+
+    let mut c = 0;
+    if let Some(p) = p1 {
+        c |= pixel_map[p as usize][0];
+    }
+
+    if let Some(p) = p2 {
+        c |= pixel_map[p as usize][1];
+    }
+
+    char::from_u32((0x2800 + c) as u32).unwrap()
+}
+
+fn fft_shift<T: Clone>(spec: &mut [T]) {
+    let spec_copy = spec.to_owned();
+
+    let (first_half, last_half) = spec_copy.split_at((spec_copy.len() + 1) / 2);
+    let shifted_spec = last_half.iter().chain(first_half.iter());
+    for (x, y) in spec.iter_mut().zip(shifted_spec) {
+        *x = y.clone();
+    }
+}
+
+fn spectrum_to_bin_heights(spec: &[Complex<f32>], dest: &mut [f32]) {
+    // subsample
+    let mut last_idx = -1isize;
+    for (i, x) in spec.iter().map(|x| x.norm()).enumerate() {
+        if (i * dest.len() / spec.len()) as isize > last_idx {
+            last_idx += 1;
+            dest[last_idx as usize] = x;
+        }
+    }
+
+    fft_shift(dest);
+}
+
+pub fn draw_spectrum(term: &mut Terminal, spec: Vec<Complex<f32>>) {
+    term.clear().unwrap();
+    let (num_cols, num_rows) = term.size();
+    let pixel_height = num_rows * 4;
+    let pixel_width = num_cols * 2;
+    // TODO what should this value be?
+    let max_height = 500.0;
+
+    let mut bins = vec![0.0; pixel_width];
+    spectrum_to_bin_heights(&spec[..], &mut bins[..]);
+
+    for col_idx in 0..num_cols {
+        // height in float between 0 and 1.
+        let h1 = bins[col_idx * 2] / max_height;
+        let h2 = bins[col_idx * 2 + 1] / max_height;
+
+        // The "pixel" height of each point.
+        let p1 = (h1 * pixel_height as f32).floor() as usize;
+        let p2 = (h2 * pixel_height as f32).floor() as usize;
+        let p1 = if p1 >= pixel_height { pixel_height - 1} else { p1 };
+        let p2 = if p2 >= pixel_height { pixel_height - 1} else { p2 };
+
+        // Reverse it, since the terminal indexing is from the top
+        let p1 = pixel_height - p1 - 1;
+        let p2 = pixel_height - p2 - 1;
+
+        let c1 = p1 / 4;
+        let c2 = p2 / 4;
+
+        if c1 == c2 {
+            term[(col_idx, c1)] = Cell::with_char(
+                pixel_nums_to_braille(Some((p1 % 4) as u8), Some((p2 % 4) as u8)));
+        } else {
+            term[(col_idx, c1)] = Cell::with_char(
+                pixel_nums_to_braille(Some((p1 % 4) as u8), None));
+            term[(col_idx, c2)] = Cell::with_char(
+                pixel_nums_to_braille(None, Some((p2 % 4) as u8)));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{pixel_nums_to_braille, fft_shift};
+
+    #[test]
+    fn test_fft_shift_dc() {
+        let len = 9;
+        let mut spec = vec![0; len];
+        spec[0] = 1;
+        fft_shift(&mut spec[..]);
+        assert_eq!(spec[len / 2], 1);
+    }
+
+    #[test]
+    fn test_fft_shift_even() {
+        let mut before: Vec<usize> = (0..10).collect();
+        let after = vec![5, 6, 7, 8, 9, 0, 1, 2, 3, 4];
+        fft_shift(&mut before[..]);
+        assert_eq!(before, after);
+    }
+
+    #[test]
+    fn test_pixel_nums() {
+        assert_eq!(pixel_nums_to_braille(Some(0), Some(0)), '⠉');
+        assert_eq!(pixel_nums_to_braille(Some(1), Some(2)), '⠢');
+        assert_eq!(pixel_nums_to_braille(None, Some(3)), '⢀');
+        assert_eq!(pixel_nums_to_braille(Some(2), None), '⠄');
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,16 @@ extern crate rustc_serialize;
 extern crate docopt;
 
 mod radio;
+mod drawing;
 
 use std::sync::mpsc::{Receiver, Sender, channel};
-use std::char;
 use num::Complex;
 use rustfft::FFT;
-use rustty::{Terminal, Cell, Event};
+use rustty::{Terminal, Event};
 use docopt::Docopt;
 
 use radio::hackrf::HackRF;
+use drawing::draw_spectrum;
 
 const USAGE: &'static str = "
 Terminal Spectrograph
@@ -54,91 +55,6 @@ fn process_buffer(recv: Receiver<Vec<Complex<i8>>>, send: Sender<Vec<Complex<f32
                 }
                 signal.clear();
             }
-        }
-    }
-}
-
-fn pixel_nums_to_braille(p1: Option<u8>, p2: Option<u8>) -> char {
-    let pixel_map = [[0x01, 0x08],
-                     [0x02, 0x10],
-                     [0x04, 0x20],
-                     [0x40, 0x80]];
-
-    let mut c = 0;
-    if let Some(p) = p1 {
-        c |= pixel_map[p as usize][0];
-    }
-
-    if let Some(p) = p2 {
-        c |= pixel_map[p as usize][1];
-    }
-
-    char::from_u32((0x2800 + c) as u32).unwrap()
-}
-
-fn bin_heights(source: &[Complex<f32>], dest: &mut [f32]) {
-    let samples_per_bin = source.len() / dest.len();
-    let mut height = 0.0;
-    let mut i = 0;
-    let mut j = 0;
-    for x in source {
-        height += x.norm();
-        if i >= samples_per_bin {
-            dest[j] = height / samples_per_bin as f32;
-            i = 0;
-            j += 1;
-            height = 0.0;
-        } else {
-            i += 1;
-        }
-    }
-
-    if i != 0 {
-        dest[j] = height / i as f32;
-    }
-}
-
-fn draw_spectrum(term: &mut Terminal, spec: Vec<Complex<f32>>) {
-    term.clear().unwrap();
-    let (num_cols, num_rows) = term.size();
-    // TODO what should this max height be?
-    let num_rows = if num_rows > 20 { 20 } else { num_rows };
-    let pixel_height = num_rows * 4;
-    let pixel_width = num_cols * 2;
-    // TODO what should this value be?
-    let max_height = 500.0;
-
-    let mut bins = vec![0.0; pixel_width];
-    bin_heights(&spec[..], &mut bins[..]);
-
-    for col_idx in 0..num_cols {
-        // height in float between 0 and 1.
-        let h1 = bins[col_idx * 2] / max_height;
-        let h2 = bins[col_idx * 2 + 1] / max_height;
-
-        // The "pixel" height of each point.
-        let p1 = (h1 * pixel_height as f32).floor() as usize;
-        let p2 = (h2 * pixel_height as f32).floor() as usize;
-        let p1 = if p1 >= pixel_height { pixel_height - 1 } else { p1 };
-        let p2 = if p2 >= pixel_height { pixel_height - 1 } else { p2 };
-
-        // Reverse it, since the terminal indexing is from the top
-        let p1 = pixel_height - p1;
-        let p2 = pixel_height - p2;
-
-        let c1 = p1 / 4;
-        let c2 = p2 / 4;
-        //let c1 = num_rows - p1 / 4;
-        //let c2 = num_rows - p2 / 4;
-
-        if c1 == c2 {
-            term[(col_idx, c1)] = Cell::with_char(
-                pixel_nums_to_braille(Some((p1 % 4) as u8), Some((p2 % 4) as u8)));
-        } else {
-            term[(col_idx, c1)] = Cell::with_char(
-                pixel_nums_to_braille(Some((p1 % 4) as u8), None));
-            term[(col_idx, c2)] = Cell::with_char(
-                pixel_nums_to_braille(None, Some((p2 % 4) as u8)));
         }
     }
 }
